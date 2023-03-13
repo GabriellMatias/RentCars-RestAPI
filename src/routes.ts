d9bf6b35fb545a916a149dca2110e09b1059e606
GabriellMatias <gabriel.goiasat@gmail.com>
2023-03-13 17:42:30 -0300

    :hammer: Chore: Adding typescript into the project

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import CreateCourseServer from "./CreateCourseServer";
+
+export function createCourse(request:Request, response:Response){
+  CreateCourseServer.execute({
+    name:"NodeJS", 
+    duration:10,
+    educator:"Matias"
+  })
+  return response.send()
+}
